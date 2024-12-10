4d470871e3b69a103039a8bb2c576d053fa55c06
KS_ABBAS <145643357+KSABBAS@users.noreply.github.com>
2024-12-10 03:27:02 +0200

    added MyResponsive

--- my_tools_development/lib/MyTools/MyFunctionTools.dart
+++ my_tools_development/lib/MyTools/MyFunctionTools.dart
@@ -203,6 +203,20 @@ double ResponsiveHeight(BuildContext context, double containerHeight, {double de
 }
 
 
+class MyResponsive {
+  final double _widthFactor;
+  final double _heightFactor;
+  
+  MyResponsive(BuildContext context, {double designWidth = 1536, double designHeight = 792})
+      : _widthFactor = MediaQuery.of(context).size.width / designWidth,
+        _heightFactor = MediaQuery.of(context).size.height / designHeight;
+
+  double fontSizeByWidth(double fontSize) => fontSize * _widthFactor;
+  double fontSizeByHeight(double fontSize) => fontSize * _heightFactor;
+  double width(double value) => value * _widthFactor;
+  double height(double value) => value * _heightFactor;
+}
+
 
 
 //===========================================
--- my_tools_development/lib/main.dart
+++ my_tools_development/lib/main.dart
@@ -12,37 +12,42 @@ class App extends StatefulWidget {
   @override
   State<App> createState() => _AppState();
 }
+late MyResponsive Res; 
 
 class _AppState extends State<App> {
   @override
   Widget build(BuildContext context) {
+    Res = MyResponsive(context);
     print(MediaQuery.of(context).size.height);
     print(MediaQuery.of(context).size.width);
     return MaterialApp(
         home: Scaffold(
-      body: Center(
+      body: CMaker(
+        height: double.infinity,
+        width: double.infinity,
         child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            // CMaker(
-            //   height: ResponsiveHeight(context, 600), //792),
-            //   width: ResponsiveWidth(context, 600), //1536),
-            //   color: const Color.fromARGB(255, 104, 187, 22),
-            //   child: TMaker(
-            //       text: "Hi Kimo",
-            //       fontSize: ResponsiveFontSizeByWidth(context, 80),
-            //       fontWeight: FontWeight.w500,
-            //       color: Colors.black),
-            // ),
-            // CMaker(
-            //   height: 200,
-            //   width: 200,
-            //   color: const Color.fromARGB(255, 104, 187, 22),
-            //   child: TMaker(
-            //       text: "Hi Kimo",
-            //       fontSize: 20,
-            //       fontWeight: FontWeight.w500,
-            //       color: Colors.black),
-            // ),
+            CMaker(
+              height: Res.height(200), //792),
+              width:Res.width(768), //1536),
+              color: const Color.fromARGB(255, 104, 187, 22),
+              child: TMaker(
+                  text: "Hi Kimo",
+                  fontSize:Res.fontSizeByWidth(40),
+                  fontWeight: FontWeight.w500,
+                  color: Colors.black),
+            ),
+            CMaker(
+              height: 200,
+              width: 200,
+              color: const Color.fromARGB(255, 104, 187, 22),
+              child: TMaker(
+                  text: "Hi Kimo",
+                  fontSize: 20,
+                  fontWeight: FontWeight.w500,
+                  color: Colors.black),
+            ),
           ],
         ),
       ),
