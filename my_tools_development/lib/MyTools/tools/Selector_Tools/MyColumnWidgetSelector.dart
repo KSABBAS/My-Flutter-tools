4cabf648d11e85db1056321048d65a7b18d7616f
KS_ABBAS <145643357+KSABBAS@users.noreply.github.com>
2025-01-26 20:29:27 +0200

    Removed extra parameters

--- my_tools_development/lib/MyTools/tools/Selector_Tools/MyColumnWidgetSelector.dart
+++ my_tools_development/lib/MyTools/tools/Selector_Tools/MyColumnWidgetSelector.dart
@@ -1,158 +1,156 @@
 import 'package:flutter/material.dart';
+import 'package:my_tools_development/MyTools/tools/CMaker_Tools/AnimatedCMaker.dart';
+import 'package:my_tools_development/MyTools/tools/CMaker_Tools/CMaker.dart';
 // import 'package:my_tools_development/MyTools/tools/CMaker_Tools/AnimatedCMaker.dart';
 // import 'package:my_tools_development/MyTools/tools/CMaker_Tools/CMaker.dart';
 
-// class MyColumnWidgetSelector extends StatefulWidget {
-//   MyColumnWidgetSelector({
-//     super.key,
-//     required this.iconsList,
-//     this.orientation,
-//     required this.height,
-//     required this.width,
-//     this.barColor,
-//     this.selectedContainerColor,
-//     this.pageBackgroundColor,
-//     this.unselectedContainerColor,
-//     this.SelectionContainerHeight,
-//     this.unSelectionContainerHeight,
-//     this.SelectionContainerWidth,
-//     this.unSelectionContainerWidth,
-//     this.SelectionContainerPadding,
-//     this.unSelectionContainerPadding,
-//     this.BarShadow,
-//     this.BarBorder,
-//     this.BarCircularRadius,
-//     this.BarGradient,
-//     this.SelectedContainerBorder,
-//     this.unSelectedContainerBorder,
-//     this.SelectionContainerCircularRadius,
-//     this.unSelectionContainerCircularRadius,
-//     this.SelectionContainerGradient,
-//     this.unSelectionContainerGradient,
-//     required this.onChange,
-//     this.ScrollDuration,
-//     this.SelectionContainerAnimationDuration,
-//     this.BetweenSpaces,
-//     this.reverseDirection
-//   });
-//   List<Widget> iconsList;
-//   String? orientation;
-//   Function(int? index) onChange;
-//   double height;
-//   double width;
-//   double? SelectionContainerHeight;
-//   double? unSelectionContainerHeight;
-//   Duration? SelectionContainerAnimationDuration;
-//   double? SelectionContainerWidth;
-//   double? unSelectionContainerWidth;
-//   double? SelectionContainerPadding;
-//   double? unSelectionContainerPadding;
-//   double? SelectionContainerCircularRadius;
-//   double? unSelectionContainerCircularRadius;
-//   double? BarCircularRadius;
-//   BoxBorder? SelectedContainerBorder;
-//   BoxBorder? unSelectedContainerBorder;
-//   Gradient? SelectionContainerGradient;
-//   Gradient? unSelectionContainerGradient;
-//   BoxBorder? BarBorder;
-//   Gradient? BarGradient;
-//   Duration? ScrollDuration;
-//   Color? barColor;
-//   Color? selectedContainerColor;
-//   Color? unselectedContainerColor;
-//   Color? pageBackgroundColor;
-//   List<BoxShadow>? BarShadow;
-//   double? BetweenSpaces;
-//   bool? reverseDirection;
-//   @override
-//   State<MyColumnWidgetSelector> createState() => _MyColumnWidgetSelectorState();
-// }
+class MyColumnWidgetSelector extends StatefulWidget {
+  MyColumnWidgetSelector({
+    super.key,
+    required this.iconsList,
+    required this.height,
+    required this.width,
+    this.barColor,
+    this.selectedContainerColor,
+    this.unselectedContainerColor,
+    this.SelectionContainerHeight,
+    this.unSelectionContainerHeight,
+    this.SelectionContainerWidth,
+    this.unSelectionContainerWidth,
+    this.SelectionContainerPadding,
+    this.unSelectionContainerPadding,
+    this.BarShadow,
+    this.BarBorder,
+    this.BarCircularRadius,
+    this.BarGradient,
+    this.SelectedContainerBorder,
+    this.unSelectedContainerBorder,
+    this.SelectionContainerCircularRadius,
+    this.unSelectionContainerCircularRadius,
+    this.SelectionContainerGradient,
+    this.unSelectionContainerGradient,
+    required this.onChange,
+    this.ScrollDuration,
+    this.SelectionContainerAnimationDuration,
+    this.BetweenSpaces,
+    this.reverseDirection
+  });
+  List<Widget> iconsList;
+  Function(int? index) onChange;
+  double height;
+  double width;
+  double? SelectionContainerHeight;
+  double? unSelectionContainerHeight;
+  Duration? SelectionContainerAnimationDuration;
+  double? SelectionContainerWidth;
+  double? unSelectionContainerWidth;
+  double? SelectionContainerPadding;
+  double? unSelectionContainerPadding;
+  double? SelectionContainerCircularRadius;
+  double? unSelectionContainerCircularRadius;
+  double? BarCircularRadius;
+  BoxBorder? SelectedContainerBorder;
+  BoxBorder? unSelectedContainerBorder;
+  Gradient? SelectionContainerGradient;
+  Gradient? unSelectionContainerGradient;
+  BoxBorder? BarBorder;
+  Gradient? BarGradient;
+  Duration? ScrollDuration;
+  Color? barColor;
+  Color? selectedContainerColor;
+  Color? unselectedContainerColor;
+  List<BoxShadow>? BarShadow;
+  double? BetweenSpaces;
+  bool? reverseDirection;
+  @override
+  State<MyColumnWidgetSelector> createState() => _MyColumnWidgetSelectorState();
+}
 
-// class _MyColumnWidgetSelectorState extends State<MyColumnWidgetSelector> {
-//   int PageIndex = 0;
-//   @override
-//   Widget build(BuildContext context) {
-//     return CMaker(
-//       boxShadow: widget.BarShadow,
-//       circularRadius: widget.BarCircularRadius ?? 20,
-//       border: widget.BarBorder,
-//       alignment: Alignment.center,
-//       gradient: widget.BarGradient,
-//       color: widget.barColor ?? Colors.white,
-//       height: widget.height,
-//       width: widget.width,
-//       child: SizedBox(
-//         width: widget.SelectionContainerWidth,
-//         child: Column(
-//           children: [
-//             Container(
-//               height: widget.BetweenSpaces ??
-//                   (widget.height -
-//                           (widget.iconsList.length *
-//                               (widget.SelectionContainerHeight ?? 60))) /
-//                       (widget.iconsList.length + 1),
-//             ),
-//             CMaker(
-//               boxShadow: widget.BarShadow,
-//               height: widget.height -
-//                   (widget.height -
-//                           (widget.iconsList.length *
-//                               (widget.SelectionContainerHeight ?? 60))) /
-//                       (widget.iconsList.length + 1),
-//               child: ListView.builder(
-//                 reverse:widget.reverseDirection?? false,
-//                 scrollDirection: Axis.vertical,
-//                 physics: const NeverScrollableScrollPhysics(),
-//                 shrinkWrap: false,
-//                 itemCount: widget.iconsList.length,
-//                 itemBuilder: (context, index) {
-//                   return Column(
-//                     children: [
-//                       InkWell(
-//                         onTap: () {
-//                           PageIndex = index;
-//                           widget.onChange(index);
-//                         },
-//                         child: CMaker(
-//                             alignment: Alignment.center,
-//                             child: ACMaker(
-//                                 duration:
-//                                     widget.SelectionContainerAnimationDuration,
-//                                 padding: EdgeInsets.all(
-//                                     widget.SelectionContainerPadding ?? 0),
-//                                 alignment: Alignment.center,
-//                                 height: widget.SelectionContainerHeight ?? 60,
-//                                 width: widget.SelectionContainerWidth ?? 60,
-//                                 circularRadius:
-//                                     widget.SelectionContainerCircularRadius ??
-//                                         15,
-//                                 border: (PageIndex == index)
-//                                     ? widget.SelectedContainerBorder
-//                                     : widget.unSelectedContainerBorder,
-//                                 gradient: widget.SelectionContainerGradient,
-//                                 color: (PageIndex == index)
-//                                     ? widget.selectedContainerColor ??
-//                                         const Color.fromARGB(255, 0, 0, 0)
-//                                     : widget.unselectedContainerColor ??
-//                                         Colors.transparent,
-//                                 child: widget.iconsList[index])),
-//                       ),
-//                       Container(
-//                         height: widget.BetweenSpaces ??
-//                             (widget.height -
-//                                     (widget.iconsList.length *
-//                                         (widget.SelectionContainerHeight ??
-//                                             60))) /
-//                                 (widget.iconsList.length + 1),
-//                       )
-//                     ],
-//                   );
-//                 },
-//               ),
-//             ),
-//           ],
-//         ),
-//       ),
-//     );
-//   }
-// }
+class _MyColumnWidgetSelectorState extends State<MyColumnWidgetSelector> {
+  int PageIndex = 0;
+  @override
+  Widget build(BuildContext context) {
+    return CMaker(
+      boxShadow: widget.BarShadow,
+      circularRadius: widget.BarCircularRadius ?? 20,
+      border: widget.BarBorder,
+      alignment: Alignment.center,
+      gradient: widget.BarGradient,
+      color: widget.barColor ?? Colors.white,
+      height: widget.height,
+      width: widget.width,
+      child: SizedBox(
+        width: widget.SelectionContainerWidth,
+        child: Column(
+          children: [
+            Container(
+              height: widget.BetweenSpaces ??
+                  (widget.height -
+                          (widget.iconsList.length *
+                              (widget.SelectionContainerHeight ?? 60))) /
+                      (widget.iconsList.length + 1),
+            ),
+            CMaker(
+              boxShadow: widget.BarShadow,
+              height: widget.height -
+                  (widget.height -
+                          (widget.iconsList.length *
+                              (widget.SelectionContainerHeight ?? 60))) /
+                      (widget.iconsList.length + 1),
+              child: ListView.builder(
+                reverse:widget.reverseDirection?? false,
+                scrollDirection: Axis.vertical,
+                physics: const NeverScrollableScrollPhysics(),
+                shrinkWrap: false,
+                itemCount: widget.iconsList.length,
+                itemBuilder: (context, index) {
+                  return Column(
+                    children: [
+                      InkWell(
+                        onTap: () {
+                          PageIndex = index;
+                          widget.onChange(index);
+                        },
+                        child: CMaker(
+                            alignment: Alignment.center,
+                            child: ACMaker(
+                                duration:
+                                    widget.SelectionContainerAnimationDuration,
+                                padding: EdgeInsets.all(
+                                    widget.SelectionContainerPadding ?? 0),
+                                alignment: Alignment.center,
+                                height: widget.SelectionContainerHeight ?? 60,
+                                width: widget.SelectionContainerWidth ?? 60,
+                                circularRadius:
+                                    widget.SelectionContainerCircularRadius ??
+                                        15,
+                                border: (PageIndex == index)
+                                    ? widget.SelectedContainerBorder
+                                    : widget.unSelectedContainerBorder,
+                                gradient: widget.SelectionContainerGradient,
+                                color: (PageIndex == index)
+                                    ? widget.selectedContainerColor ??
+                                        const Color.fromARGB(255, 0, 0, 0)
+                                    : widget.unselectedContainerColor ??
+                                        Colors.transparent,
+                                child: widget.iconsList[index])),
+                      ),
+                      Container(
+                        height: widget.BetweenSpaces ??
+                            (widget.height -
+                                    (widget.iconsList.length *
+                                        (widget.SelectionContainerHeight ??
+                                            60))) /
+                                (widget.iconsList.length + 1),
+                      )
+                    ],
+                  );
+                },
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
--- my_tools_development/lib/MyTools/tools/Selector_Tools/MyExpandingRowWidgetSelector.dart
+++ my_tools_development/lib/MyTools/tools/Selector_Tools/MyExpandingRowWidgetSelector.dart
@@ -1,128 +1,124 @@
 import 'package:flutter/material.dart';
-// import 'package:my_tools_development/MyTools/tools/CMaker_Tools/AnimatedCMaker.dart';
-// import 'package:my_tools_development/MyTools/tools/CMaker_Tools/CMaker.dart';
+import 'package:my_tools_development/MyTools/tools/CMaker_Tools/AnimatedCMaker.dart';
+import 'package:my_tools_development/MyTools/tools/CMaker_Tools/CMaker.dart';
 
-// class MyExpandingRowWidgetSelector extends StatefulWidget {
-//   MyExpandingRowWidgetSelector({
-//     super.key,
-//     required this.iconsList,
-//     this.orientation,
-//     required this.height, // Only specify height, width will be dynamic
-//     this.barColor,
-//     this.selectedContainerColor,
-//     this.pageBackgroundColor,
-//     this.unselectedContainerColor,
-//     this.SelectionContainerHeight,
-//     this.unSelectionContainerHeight,
-//     this.SelectionContainerWidth,
-//     this.unSelectionContainerWidth,
-//     this.SelectionContainerPadding,
-//     this.unSelectionContainerPadding,
-//     this.BarShadow,
-//     this.BarBorder,
-//     this.BarCircularRadius,
-//     this.BarGradient,
-//     this.SelectedContainerBorder,
-//     this.unSelectedContainerBorder,
-//     this.SelectionContainerCircularRadius,
-//     this.unSelectionContainerCircularRadius,
-//     this.SelectionContainerGradient,
-//     this.unSelectionContainerGradient,
-//     required this.onChange,
-//     this.ScrollDuration,
-//     this.SelectionContainerAnimationDuration,
-//     this.betweenSpaces, // New parameter for spacing between widgets
-//     this.reverseDirection,
-//   });
+class MyExpandingRowWidgetSelector extends StatefulWidget {
+  MyExpandingRowWidgetSelector({
+    super.key,
+    required this.iconsList,
+    required this.height, // Only specify height, width will be dynamic
+    this.barColor,
+    this.selectedContainerColor,
+    this.unselectedContainerColor,
+    this.SelectionContainerHeight,
+    this.unSelectionContainerHeight,
+    this.SelectionContainerWidth,
+    this.unSelectionContainerWidth,
+    this.SelectionContainerPadding,
+    this.unSelectionContainerPadding,
+    this.BarShadow,
+    this.BarBorder,
+    this.BarCircularRadius,
+    this.BarGradient,
+    this.SelectedContainerBorder,
+    this.unSelectedContainerBorder,
+    this.SelectionContainerCircularRadius,
+    this.unSelectionContainerCircularRadius,
+    this.SelectionContainerGradient,
+    this.unSelectionContainerGradient,
+    required this.onChange,
+    this.ScrollDuration,
+    this.SelectionContainerAnimationDuration,
+    this.betweenSpaces, // New parameter for spacing between widgets
+    this.reverseDirection,
+  });
 
-//   final List<Widget> iconsList;
-//   final String? orientation;
-//   final Function(int? index) onChange;
-//   final double height; // Only height is specified, width is dynamic
-//   final double? SelectionContainerHeight;
-//   final double? unSelectionContainerHeight;
-//   final Duration? SelectionContainerAnimationDuration;
-//   final double? SelectionContainerWidth;
-//   final double? unSelectionContainerWidth;
-//   final double? SelectionContainerPadding;
-//   final double? unSelectionContainerPadding;
-//   final double? SelectionContainerCircularRadius;
-//   final double? unSelectionContainerCircularRadius;
-//   final double? BarCircularRadius;
-//   final BoxBorder? SelectedContainerBorder;
-//   final BoxBorder? unSelectedContainerBorder;
-//   final Gradient? SelectionContainerGradient;
-//   final Gradient? unSelectionContainerGradient;
-//   final BoxBorder? BarBorder;
-//   final Gradient? BarGradient;
-//   final Duration? ScrollDuration;
-//   final Color? barColor;
-//   final Color? selectedContainerColor;
-//   final Color? unselectedContainerColor;
-//   final Color? pageBackgroundColor;
-//   final List<BoxShadow>? BarShadow;
-//   final double? betweenSpaces; // Spacing between widgets
-//   final bool? reverseDirection;
+  final List<Widget> iconsList;
+  final Function(int? index) onChange;
+  final double height; // Only height is specified, width is dynamic
+  final double? SelectionContainerHeight;
+  final double? unSelectionContainerHeight;
+  final Duration? SelectionContainerAnimationDuration;
+  final double? SelectionContainerWidth;
+  final double? unSelectionContainerWidth;
+  final double? SelectionContainerPadding;
+  final double? unSelectionContainerPadding;
+  final double? SelectionContainerCircularRadius;
+  final double? unSelectionContainerCircularRadius;
+  final double? BarCircularRadius;
+  final BoxBorder? SelectedContainerBorder;
+  final BoxBorder? unSelectedContainerBorder;
+  final Gradient? SelectionContainerGradient;
+  final Gradient? unSelectionContainerGradient;
+  final BoxBorder? BarBorder;
+  final Gradient? BarGradient;
+  final Duration? ScrollDuration;
+  final Color? barColor;
+  final Color? selectedContainerColor;
+  final Color? unselectedContainerColor;
+  final List<BoxShadow>? BarShadow;
+  final double? betweenSpaces; // Spacing between widgets
+  final bool? reverseDirection;
 
-//   @override
-//   State<MyExpandingRowWidgetSelector> createState() => _MyExpandingRowWidgetSelectorState();
-// }
+  @override
+  State<MyExpandingRowWidgetSelector> createState() => _MyExpandingRowWidgetSelectorState();
+}
 
-// class _MyExpandingRowWidgetSelectorState extends State<MyExpandingRowWidgetSelector> {
-//   int PageIndex = 0;
+class _MyExpandingRowWidgetSelectorState extends State<MyExpandingRowWidgetSelector> {
+  int PageIndex = 0;
 
-//   @override
-//   Widget build(BuildContext context) {
-//     return CMaker(
-//       boxShadow: widget.BarShadow,
-//       circularRadius: widget.BarCircularRadius ?? 20,
-//       border: widget.BarBorder,
-//       alignment: Alignment.center,
-//       gradient: widget.BarGradient,
-//       color: widget.barColor ?? Colors.white,
-//       height: widget.height, // Set the height explicitly
-//       child: ListView.builder(
-//         scrollDirection: Axis.horizontal,
-//         reverse: widget.reverseDirection ?? false, // Enable reverse scrolling
-//         itemCount: widget.iconsList.length,
-//         itemBuilder: (context, index) {
-//           return Padding(
-//             padding: EdgeInsets.only(
-//               left:  widget.betweenSpaces ??0,
-//               right: index == widget.iconsList.length - 1 && widget.reverseDirection == true
-//                   ? widget.betweenSpaces ?? 0
-//                   : widget.betweenSpaces ?? 0,
-//             ),
-//             child: InkWell(
-//               onTap: () {
-//                 setState(() {
-//                   PageIndex = index;
-//                 });
-//                 widget.onChange(PageIndex);
-//               },
-//               child: CMaker(
-//                 alignment: Alignment.center,
-//                 child: ACMaker(
-//                   duration: widget.SelectionContainerAnimationDuration,
-//                   padding: EdgeInsets.all(widget.SelectionContainerPadding ?? 0),
-//                   alignment: Alignment.center,
-//                   height: widget.SelectionContainerHeight ?? 60,
-//                   width: widget.SelectionContainerWidth ?? 60,
-//                   circularRadius: widget.SelectionContainerCircularRadius ?? 15,
-//                   border: (PageIndex == index)
-//                       ? widget.SelectedContainerBorder
-//                       : widget.unSelectedContainerBorder,
-//                   gradient: widget.SelectionContainerGradient,
-//                   color: (PageIndex == index)
-//                       ? widget.selectedContainerColor ?? const Color.fromARGB(255, 0, 0, 0)
-//                       : widget.unselectedContainerColor ?? Colors.transparent,
-//                   child: widget.iconsList[index],
-//                 ),
-//               ),
-//             ),
-//           );
-//         },
-//       ),
-//     );
-//   }
-// }
+  @override
+  Widget build(BuildContext context) {
+    return CMaker(
+      boxShadow: widget.BarShadow,
+      circularRadius: widget.BarCircularRadius ?? 20,
+      border: widget.BarBorder,
+      alignment: Alignment.center,
+      gradient: widget.BarGradient,
+      color: widget.barColor ?? Colors.white,
+      height: widget.height, // Set the height explicitly
+      child: ListView.builder(
+        scrollDirection: Axis.horizontal,
+        reverse: widget.reverseDirection ?? false, // Enable reverse scrolling
+        itemCount: widget.iconsList.length,
+        itemBuilder: (context, index) {
+          return Padding(
+            padding: EdgeInsets.only(
+              left:  widget.betweenSpaces ??0,
+              right: index == widget.iconsList.length - 1 && widget.reverseDirection == true
+                  ? widget.betweenSpaces ?? 0
+                  : widget.betweenSpaces ?? 0,
+            ),
+            child: InkWell(
+              onTap: () {
+                setState(() {
+                  PageIndex = index;
+                });
+                widget.onChange(PageIndex);
+              },
+              child: CMaker(
+                alignment: Alignment.center,
+                child: ACMaker(
+                  duration: widget.SelectionContainerAnimationDuration,
+                  padding: EdgeInsets.all(widget.SelectionContainerPadding ?? 0),
+                  alignment: Alignment.center,
+                  height: widget.SelectionContainerHeight ?? 60,
+                  width: widget.SelectionContainerWidth ?? 60,
+                  circularRadius: widget.SelectionContainerCircularRadius ?? 15,
+                  border: (PageIndex == index)
+                      ? widget.SelectedContainerBorder
+                      : widget.unSelectedContainerBorder,
+                  gradient: widget.SelectionContainerGradient,
+                  color: (PageIndex == index)
+                      ? widget.selectedContainerColor ?? const Color.fromARGB(255, 0, 0, 0)
+                      : widget.unselectedContainerColor ?? Colors.transparent,
+                  child: widget.iconsList[index],
+                ),
+              ),
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
